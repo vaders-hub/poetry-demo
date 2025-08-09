c65cc00d05a337d561a55d2689732989d7afd1d3
Hyunbae Jeon <hyunbaejeon@Hyunbaes-MacBook-Pro.local>
2025-08-09 15:47:50 +0900

    log

--- src/main.py
+++ src/main.py
@@ -6,14 +6,18 @@ from src.db import sessionmanager
 from fastapi import FastAPI
 import uvicorn
 
-logging.basicConfig(stream=sys.stdout, level=logging.DEBUG if setting.log_level == "DEBUG" else logging.INFO)
+logging.basicConfig(
+    stream=sys.stdout,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.DEBUG if setting.log_level == "DEBUG" else logging.INFO,
+    # handlers=[logging.StreamHandler()]
+)
 
 def start():
     try:
         uvicorn.run("src.router:app", host="0.0.0.0", port=8001, reload=True)
     except uvicorn.Error as error:
         logging.critical(f"An unhandled uvicorn error occurred: {error}")
-
 
 if __name__ == "__main__":
     start()
